e125bcdae86fc072c05d797e31d81961bdafb3e8
illusionTBA <illusionztba@gmail.com>
2022-08-22 16:26:16 -0400

    animations

--- src/App.js
+++ src/App.js
@@ -2,16 +2,18 @@
 import {
   Routes,
   Route,
+  useLocation
 } from 'react-router-dom';
 import { AnimatePresence, motion } from 'framer-motion';
 import Home from "./pages/Home";
 import Play from './pages/Play';
 import Games from './pages/Games';
 function App() {
+  const location = useLocation();
   return (
     <>
       <AnimatePresence exitBeforeEnter>
-        <Routes>
+        <Routes location={location} key={location.key}>
           <Route path="/" element={<Home />} />
           <Route path="/games" element={<Games />} />
           <Route path='play/:id' element={<Play />} />
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,24 +1,35 @@
 /* eslint-disable */
 import React from 'react';
-import useScript from '../components/scripts/useScript';
 import gameList from '../games.json';
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { motion } from 'framer-motion';
-
+import { NavLink } from 'react-router-dom';
 import { TbBrandGithub } from 'react-icons/tb';
 import { FaDiscord } from 'react-icons/fa';
-function Home() {
-  // injecting ruffle js into DOM
-  useScript('/ruffle/ruffle.js');
 
+const pageTransition = {
+  type: 'tween',
+  ease: 'anticipate',
+  duration: 0.5,
+};
+
+const pageVariants = {
+  initial: { opacity: 0, x: '100vh' },
+  animate: { opacity: 1, x: 0 },
+  exit: { opacity: 0, x: '-100vh' },
+};
+
+function Home() {
   const [games, setGames] = useState(gameList.length);
 
   return (
     <div className="flex flex-col items-center w-full h-screen overflow-hidden">
       <motion.div
-        initial={{ opacity: 0, y: 100 }}
-        animate={{ opacity: 1, y: 0 }}
-        exit={{ opacity: 0, y: 100 }}
+        variants={pageVariants}
+        initial="initial"
+        animate="animate"
+        exit="exit"
+        transition={pageTransition}
         className="flex flex-col w-full h-full justify-center items-center"
       >
         <img src="/favicon.png" width={200} className="drop-shadow-xl" />
@@ -28,12 +39,12 @@ function Home() {
           We have over <span className="text-1xl text-gray-400">{games}+</span>{' '}
           games for you to choose from
         </p>
-        <a
-          href="/games"
+        <NavLink
+          to="/games"
           className="inline-flex items-center justify-center drop-shadow-xl h-14 px-10 py-2 mt-2 text-xl font-semibold text-center text-gray-200 no-underline align-middle transition-all duration-300 ease-in-out bg-transparent border-2 border-gray-600 border-solid rounded-full cursor-pointer select-none hover:text-white hover:border-white focus:shadow-xs focus:no-underline"
         >
           Start Browsing
-        </a>
+        </NavLink>
       </motion.div>
       <div className="flex bottom-0 items-center">
         <div className="flex flex-row rounded-md bg-[#1f2635] p-1 space-x-4">
